b01591a860e5e8257896d5aab9a8e6d9086f272d
NJ <raju.jalapala@codification.io>
2020-06-04 22:14:37 +0100

    adding vnet module

--- modules/vnet/vnet.tf
+++ modules/vnet/vnet.tf
@@ -10,11 +10,3 @@ resource azurerm_virtual_network "vnet" {
   dns_servers         = var.dnsServers
   tags                = var.common_tags
 }
-
-resource "azurerm_subnet" "subnet" {
-  count                = length(var.subnetNames)
-  name                 = var.subnetNames[count.index]
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["${var.subnetPrefixes[count.index]}"]
-}
