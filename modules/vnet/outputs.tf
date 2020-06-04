31d494fb07b50fcf5237aae2818ace1ad1c7a414
NJ <raju.jalapala@codification.io>
2020-06-04 22:35:25 +0100

    corrected outputs.tf

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -17,9 +17,3 @@ output "vnetAddressSpace" {
   description = "The address space of the newly created vNet"
   value       = azurerm_virtual_network.vnet.address_space
 }
-
-output "vnetSubnets" {
-  description = "The ids of subnets created inside the new vNet"
-  value       = azurerm_subnet.subnet[index(azurerm_subnet.subnet.*.name, "bootstrap-services")].id
-}
-
